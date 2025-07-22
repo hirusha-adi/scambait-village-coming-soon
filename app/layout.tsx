8abdfb292e410662a7070e8af4f0483f9d4142d7
Hirusha Adikari <hirushaadi@gmail.com>
2025-07-22 14:58:05 +1000

    update site title

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Scambait Village',
+  description: 'Expect us @DEFCON 33.',
+  generator: 'scambait-village',
 }
 
 export default function RootLayout({
